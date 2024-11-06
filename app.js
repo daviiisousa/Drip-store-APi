ff02d09327a45ff4b34818f577449424dcd97342
daviiisousa <sousadavi248@gmail.com>
2024-11-06 20:05:47 -0300

    config cors

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors'); // Importa o pacote cors
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Habilita o CORS para todas as requisições
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
