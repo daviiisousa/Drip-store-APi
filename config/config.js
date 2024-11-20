ecf8bb34564e438ce8987eddaf693baf60ea2c9c
daviiisousa <sousadavi248@gmail.com>
2024-11-20 15:00:25 -0300

    removendo o env do repositorio remoto

--- config/config.js
+++ config/config.js
@@ -1,22 +1,22 @@
 require('dotenv').config()
-console.log(process.env.DB_user,process.env.DB_PASS)
+
 module.exports = {
    
-    // development:{
-    //     username: process.env.DB_USER,
-    //     host: process.env.DB_HOST,
-    //     port: process.env.DB_PORT,
-    //     password: process.env.DB_PASS,
-    //     dialect: process.env.DB_DIALECT,
-    //     database: process.env.DB_NAME
-    // }
     development:{
-        username:"root",
-        host:"localhost",
-        port:" 3306",
-        password:"2710",
-        dialect: "mariadb",
-        database: "dripapi"
+        username: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        password: process.env.DB_PASS,
+        dialect: process.env.DB_DIALECT,
+        database: process.env.DB_NAME
     }
+    // development:{
+    //     username:"root",
+    //     host:"localhost",
+    //     port:" 3306",
+    //     password:"2710",
+    //     dialect: "mariadb",
+    //     database: "dripapi"
+    // }
 
 }
